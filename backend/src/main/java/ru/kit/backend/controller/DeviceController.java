bb54da229edc875ad66b67fc68597cf64bf7c0cd
nialliv <artemjevnikita@yandex.ru>
2022-08-10 12:48:50 +0400

    refactor imports

--- backend/src/main/java/ru/kit/backend/controller/DeviceController.java
+++ backend/src/main/java/ru/kit/backend/controller/DeviceController.java
@@ -1,15 +1,21 @@
 package ru.kit.backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import ru.kit.backend.exception.DeviceAlreadyExistException;
 import ru.kit.backend.exception.DeviceNotFoundException;
 import ru.kit.backend.model.Device;
 import ru.kit.backend.service.IDeviceService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/devices")
--- backend/src/main/java/ru/kit/backend/controller/UserController.java
+++ backend/src/main/java/ru/kit/backend/controller/UserController.java
@@ -1,14 +1,21 @@
 package ru.kit.backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import ru.kit.backend.exception.UserAlreadyExistException;
 import ru.kit.backend.exception.UserNotFoundException;
 import ru.kit.backend.model.User;
 import ru.kit.backend.service.IUserService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
