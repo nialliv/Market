ab0eb1e300b409960fd0f71c83fb64f8242c7dd5
nialliv <artemjevnikita@yandex.ru>
2022-08-17 19:55:42 +0400

    fix imports

--- product-app/src/main/java/ru/kit/productapp/model/ProductBrand.java
+++ product-app/src/main/java/ru/kit/productapp/model/ProductBrand.java
@@ -3,12 +3,11 @@ package ru.kit.productapp.model;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ElementCollection;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
