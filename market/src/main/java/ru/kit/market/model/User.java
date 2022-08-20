79e540b336e2d5257fc0c30b456603996693aa79
nialliv <artemjevnikita@yandex.ru>
2022-08-20 21:51:36 +0400

    refactor

--- market/src/main/java/ru/kit/market/model/User.java
+++ market/src/main/java/ru/kit/market/model/User.java
@@ -1,9 +1,20 @@
 package ru.kit.market.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-import lombok.*;
-
-import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
@@ -14,6 +25,7 @@ import javax.persistence.*;
 @ToString
 @Table(name = "users")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -32,5 +44,4 @@ public class User {
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Role role;
-
 }
--- market/src/main/java/ru/kit/market/repository/UserRepository.java
+++ market/src/main/java/ru/kit/market/repository/UserRepository.java
@@ -2,6 +2,7 @@ package ru.kit.market.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import ru.kit.market.model.User;
 
 @Repository
